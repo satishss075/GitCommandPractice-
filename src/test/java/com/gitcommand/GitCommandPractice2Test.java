c6244867d109ce62539ac639ad44ff4a52f6138d
satishss075 <satishss075@gmail.com>
2022-07-25 12:43:06 +0530

    second

---
+++ src/test/java/com/gitcommand/GitCommandPractice2Test.java
@@ -0,0 +1,11 @@
+package com.gitcommand;
+
+public class GitCommandPractice2Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("practis is done ");
+
+	}
+
+}
